5006ef11a892a18bd1f2fb0e95a6c8eab142c9e4
usuario <usuario@com1pc28.uesocc.edu.sv>
2017-08-17 06:17:07 -0600

    Se agregan dos metodos

---
+++ guia01Git/src/main/java/sv/edu/uesocc/ingenieria/prn335_2017/guia01git/metodos.java
@@ -0,0 +1,21 @@
+package sv.edu.uesocc.ingenieria.prn335_2017.guia01git;
+
+/**
+ *
+ * @author usuario
+ */
+public class metodos {
+    
+     public String getResume(String texto){
+        return texto.substring(0, 29);
+    }
+   
+     
+     
+    public String capitalizar(String texto){
+        
+        return texto.toUpperCase();
+    }
+    
+    
+}

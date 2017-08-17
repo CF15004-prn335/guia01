80975623e10a6158cad9d8d28a9d860013dc03ab
usuario <usuario@com1pc28.uesocc.edu.sv>
2017-08-17 06:49:23 -0600

    Se comenta el codigo

--- guia01Git/src/main/java/sv/edu/uesocc/ingenieria/prn335_2017/guia01git/metodos.java
+++ guia01Git/src/main/java/sv/edu/uesocc/ingenieria/prn335_2017/guia01git/metodos.java
@@ -5,20 +5,32 @@ package sv.edu.uesocc.ingenieria.prn335_2017.guia01git;
  * @author usuario
  */
 public class metodos {
-    //Este método toma los primeros 30 caracteres de una cadena de texto
+    /**
+     * @param texto
+     * @return Los primeros 30 caracteres del texto
+     */
      public String getResume(String texto){
         return texto.substring(0, 29);
     }
    
      
-    //Este metodo convierte a mayúscula el texto que recibe
+    /**
+     * 
+     * @param texto
+     * @return  El texto en mayusculas
+     */
     public String capitalizar(String texto){
         
         return texto.toUpperCase();
     }
     
     
-    //Se compara un texto con otro(frase) y se devuelve la cantidad de coincidencias
+    /**
+     * 
+     * @param texto
+     * @param frase
+     * @return Compara texto y frase y devuelve el numero de coincidencias
+     */
     public int contarCoincidencias(String texto, String frase){
         
         return texto.compareTo(frase);
